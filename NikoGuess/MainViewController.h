15558d1751f0cfdc46b2584f43d346e3313fcdfe
Andrew Rangel <andrewjrangel@gmail.com>
2013-03-27 18:33:08 -0500

    Created outlets for buttons

---
+++ NikoGuess/MainViewController.h
@@ -0,0 +1,35 @@
+//
+//  MainViewController.h
+//  NikoGuess
+//
+//  Created by Ventura Rangel on 3/27/13.
+//  Copyright (c) 2013 Andrews Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+-(IBAction)buttonOne:(id)sender;
+-(IBAction)buttonTwo:(id)sender;
+-(IBAction)buttonThree:(id)sender;
+-(IBAction)buttonFour:(id)sender;
+-(IBAction)buttonFive:(id)sender;
+-(IBAction)buttonSix:(id)sender;
+-(IBAction)buttonSeven:(id)sender;
+-(IBAction)buttonEight:(id)sender;
+-(IBAction)buttonNine:(id)sender;
+
+@property (strong, nonatomic) IBOutlet UIButton *buttonOne;
+@property (strong, nonatomic) IBOutlet UIButton *buttonTwo;
+@property (strong, nonatomic) IBOutlet UIButton *buttonThree;
+@property (strong, nonatomic) IBOutlet UIButton *buttonFour;
+@property (strong, nonatomic) IBOutlet UIButton *buttonFive;
+@property (strong, nonatomic) IBOutlet UIButton *buttonSix;
+@property (strong, nonatomic) IBOutlet UIButton *buttonSeven;
+@property (strong, nonatomic) IBOutlet UIButton *buttonEight;
+@property (strong, nonatomic) IBOutlet UIButton *buttonNine;
+
+
+
+
+@end
---
+++ NikoGuess/MainViewController.m
@@ -0,0 +1,77 @@
+//
+//  MainViewController.m
+//  NikoGuess
+//
+//  Created by Ventura Rangel on 3/27/13.
+//  Copyright (c) 2013 Andrews Apps. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+-(IBAction)buttonOne:(id)sender{
+    
+}
+-(IBAction)buttonTwo:(id)sender{
+    
+}
+
+-(IBAction)buttonThree:(id)sender{
+    
+}
+-(IBAction)buttonFour:(id)sender{
+    
+}
+-(IBAction)buttonFive:(id)sender{
+    
+}
+-(IBAction)buttonSix:(id)sender{
+    
+}
+-(IBAction)buttonSeven:(id)sender{
+    
+}
+-(IBAction)buttonEight:(id)sender{
+    
+}
+-(IBAction)buttonNine:(id)sender{
+    
+}
+
+
+
+
+
+
+
+
+
+
+@end

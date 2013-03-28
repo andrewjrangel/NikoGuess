6efeed93c1ae0b7fb71f7fe7c8e7b6bf8708cf2f
Andrew Rangel <andrewjrangel@gmail.com>
2013-03-27 19:44:03 -0500

    Developed the ability to start the game over. Code drying up. Still need the three wins aspect.

--- NikoGuess/MainViewController.m
+++ NikoGuess/MainViewController.m
@@ -42,8 +42,44 @@ NSString *guess;
     answers = [[NSArray alloc] initWithObjects:@"1", @"2", @"3", @"4", @"6", @"7", @"8", @"9", nil];
     int myIndex = arc4random_uniform(answers.count);
     answer = [answers objectAtIndex:myIndex];
+    if (gamesPlayed  == 4){
+        [self setKey];
+    }
+    BOOL over = [[NSUserDefaults standardUserDefaults] boolForKey:@"overage"];
+    if (over) {
+        [self setAllHidden];
+        [self tooManyGuesses];
+        
+    }
     
-    
+}
+
+-(void)setAllHidden{
+    [buttonOne setHidden:YES];
+    [buttonTwo setHidden:YES];
+    [buttonThree setHidden:YES];
+    [buttonFour setHidden:YES];
+    [buttonFive setHidden:YES];
+    [buttonSix setHidden:YES];
+    [buttonSeven setHidden:YES];
+    [buttonEight setHidden:YES];
+    [buttonNine setHidden:YES];
+}
+-(void)showAll{
+    [buttonOne setHidden:NO];
+    [buttonTwo setHidden:NO];
+    [buttonThree setHidden:NO];
+    [buttonFour setHidden:NO];
+    [buttonFive setHidden:NO];
+    [buttonSix setHidden:NO];
+    [buttonSeven setHidden:NO];
+    [buttonEight setHidden:NO];
+    [buttonNine setHidden:NO];
+}
+
+-(void)setKey{
+    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"overage"];
+
 }
 
 
@@ -54,9 +90,10 @@ NSString *guess;
 }
 
 int count = 0;
+int gamesPlayed = 0;
 
 -(void)tooManyGuesses{
-    UIAlertView *overageAlert = [[UIAlertView alloc] initWithTitle:@"No More" message:@"You only have four guesses to get this cats number right!" delegate:nil cancelButtonTitle:@"Meow..." otherButtonTitles:nil, nil];
+    UIAlertView *overageAlert = [[UIAlertView alloc] initWithTitle:@"No More" message:@"You only have four guesses to get this cats number right!" delegate:nil cancelButtonTitle:@"Try again" otherButtonTitles:nil, nil];
     [overageAlert show];
 }
 
@@ -71,13 +108,15 @@ int count = 0;
     guess = @"1";
     
     if(count >=4){
-        
+        gamesPlayed = gamesPlayed +1;
         [self tooManyGuesses];
-        
+        [self showAll];
     } else if(guess == answer){
         [self correctAnswer];
+    } else {
+        [buttonOne setHidden:YES];
     }
-    [buttonOne setHidden:YES];
+    
 }
 
 
@@ -86,14 +125,17 @@ int count = 0;
     guess = @"2";
     
     if(count >=4){
-        
+        gamesPlayed = gamesPlayed +1;
         [self tooManyGuesses];
+        [self showAll];
         
     } else if(guess == answer){
         [self correctAnswer];
         
+    } else {
+       [buttonTwo setHidden:YES]; 
     }
-    [buttonTwo setHidden:YES];
+    
     
 }
 
@@ -102,14 +144,16 @@ int count = 0;
     guess = @"3";
     
     if(count >=4){
-        
+        gamesPlayed = gamesPlayed +1;
         [self tooManyGuesses];
-        
+        [self showAll];
     } else if(guess == answer){
         [self correctAnswer];
         
+    } else {
+       [buttonThree setHidden:YES]; 
     }
-    [buttonThree setHidden:YES];
+    
     
 }
 -(IBAction)buttonFour:(id)sender{
@@ -117,14 +161,16 @@ int count = 0;
     guess = @"4";
     
     if(count >=4){
-        
+        gamesPlayed = gamesPlayed +1;
         [self tooManyGuesses];
-        
+        [self showAll];
     } else if(guess == answer){
         [self correctAnswer];
         
+    } else {
+       [buttonFour setHidden:YES]; 
     }
-    [buttonFour setHidden:YES];
+    
     
 }
 -(IBAction)buttonFive:(id)sender{
@@ -132,14 +178,16 @@ int count = 0;
     guess = @"5";
     
     if(count >=4){
-        
+        gamesPlayed = gamesPlayed +1;
         [self tooManyGuesses];
-        
+        [self showAll];
     } else if(guess == answer){
         [self correctAnswer];
         
+    } else {
+        [buttonFive setHidden:YES];
     }
-    [buttonFive setHidden:YES];
+    
     
 }
 -(IBAction)buttonSix:(id)sender{
@@ -147,14 +195,16 @@ int count = 0;
     guess = @"6";
     
     if(count >=4){
-        
+        gamesPlayed = gamesPlayed +1;
         [self tooManyGuesses];
-        
+        [self showAll];
     } else if(guess == answer){
         [self correctAnswer];
         
+    } else {
+       [buttonSix setHidden:YES]; 
     }
-    [buttonSix setHidden:YES];
+    
     
 }
 -(IBAction)buttonSeven:(id)sender{
@@ -162,14 +212,15 @@ int count = 0;
     guess = @"7";
     
     if(count >=4){
-        
+        gamesPlayed = gamesPlayed +1;
         [self tooManyGuesses];
-        
+        [self showAll];
     } else if(guess == answer){
         [self correctAnswer];
         
+    } else {
+        [buttonSeven setHidden:YES];
     }
-    [buttonSeven setHidden:YES];
     
 }
 -(IBAction)buttonEight:(id)sender{
@@ -177,28 +228,30 @@ int count = 0;
     guess = @"8";
     
     if(count >=4){
-        
+        gamesPlayed = gamesPlayed +1;
         [self tooManyGuesses];
-        
+        [self showAll];
     } else if(guess == answer){
         [self correctAnswer];
         
-    }
+    } else{
     [buttonEight setHidden:YES];
+    }
 }
 -(IBAction)buttonNine:(id)sender{
     count = count +1;
     guess = @"9";
     
     if(count >=4){
-        
+        gamesPlayed = gamesPlayed +1;
         [self tooManyGuesses];
-        
+        [self showAll];
     } else if(guess == answer){
         [self correctAnswer];
         
+    } else{
+        [buttonNine setHidden:YES];
     }
-    [buttonNine setHidden:YES];
 }
 
 
